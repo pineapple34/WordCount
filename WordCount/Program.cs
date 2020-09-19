bc2d24518fc35b97cf6cc58542465e9985a6f618
pineapple34 <pineapple6934@gmail.com>
2020-09-19 23:23:47 +0500

    Initial commit

---
+++ WordCount/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите слово - ");
+            string InputWord = Console.ReadLine();
+
+            int wordCount = 0;
+            string text = System.IO.File.ReadAllText("file.txt");
+            string[] inputParsed = text.Split(' ', ',', '.') ;
+
+            foreach (string word in inputParsed)
+            {
+                if (word.ToLower() == InputWord.ToLower()) wordCount++;
+            }
+
+            Console.WriteLine("Слово {0} встречается в тексте {1} раз(а)", InputWord, wordCount);
+
+            Console.ReadKey();
+        }
+    }
+}
